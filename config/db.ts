adeca2c061b2845441846b567fa863e1f9b02b12
Aus Gomez <austrebertog@gmail.com>
2021-08-29 15:02:26 -0500

    adding pg support to db connector

--- config/db.ts
+++ config/db.ts
@@ -1,12 +1,25 @@
-import { Database, SQLite3Connector } from "https://deno.land/x/denodb/mod.ts";
+import {
+  Database,
+  PostgresConnector,
+  SQLite3Connector,
+} from "https://deno.land/x/denodb/mod.ts";
+
+// Models
 import { User } from "../models/User.ts";
 
+// Uses SQLite
 const connector = new SQLite3Connector({
   filepath: "./database.sqlite",
 });
 
+// For PGSQL
+// const connector = new PostgresConnector({
+//   host: '...',
+//   username: 'user',
+//   password: 'password',
+//   database: 'airlines',
+// });
+
 export const db = new Database(connector).link(
   [User],
 );
-
-// export db;/
